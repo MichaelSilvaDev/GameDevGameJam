3683530aea4b966b1b48e7248abc2244770d0b96
Michael Silva <michaelsilva81@hotmail.com>
2020-05-01 13:22:16 -0400

    Player Movement Demo Scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PlayerController : MonoBehaviour
+{
+
+	[SerializeField] float speed = 10.0F;
+	[SerializeField] float rotateSpeed = 10.0f;
+
+
+	void Start()
+	{
+		
+
+	}
+
+	void Update()
+	{
+		
+		PlayerInput();
+
+
+	}
+
+	
+
+	private void PlayerInput()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector3.forward * speed * Time.deltaTime * -1);
+		}
+	}
+
+	
+}
